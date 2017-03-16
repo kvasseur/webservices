8e337c7c3db35d10faba1adcbc0da008d91b7dc0
kvasseur <vasseur.kev@outlook.fr>
2017-03-16 09:13:41 +0100

    ajout page add book

---
+++ cake/app/Controller/BooksController.php
@@ -0,0 +1,26 @@
+<?php
+	
+	class BooksController extends AppController{
+
+		public function add(){
+			if($this->request->is('post')){
+
+				debug($this->request->data);
+				$name=$this->Book->findByName($this->request->data['Book']['name']);
+				
+				if(empty($name)){
+
+					$this->Book->create($this->request->data);
+					$this->Book->save(null, true);
+					debug("Insertion réussie!");
+				
+				}else{
+					debug("Erreur : enregistrement déjà existant");
+				}
+
+			}
+		}
+
+	}
+
+?>
---
+++ cake/app/View/Books/add.ctp
@@ -0,0 +1,6 @@
+<h1>Ajouter un livre</h1>
+
+<?= $this->Form->create('Book');?>
+	<?= $this->Form->input('name', array('label'=>'Nom du livre'));?>
+	<?= $this->Form->input('category', array('label'=>'Catégorie'));?>
+<?= $this->Form->end('Enregistrer le livre');?>
